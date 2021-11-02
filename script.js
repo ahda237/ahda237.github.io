f6dfffb12b223cc53e6ed75bd99b15fe369d4be7
ahda237 <93605852+ahda237@users.noreply.github.com>
2021-11-03 06:11:44 +0700

    deploy web

---
+++ script.js
@@ -0,0 +1,17 @@
+(function($) {
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(550).fadeOut('slow'); 
+		$('body').delay(450).css({'overflow':'visible'});
+	});
+}(jQuery));
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
